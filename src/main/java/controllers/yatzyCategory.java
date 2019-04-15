6534271b9b1eac1b48fd10a568b36f0e55b98cf3
Eli Sall <elimane.sall@telecomnancy.net>
2019-04-15 20:20:11 +0200

    implementation of Yatzy category controller

---
+++ src/main/java/controllers/yatzyCategory.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import domain.GivenRoll;
+
+public class yatzyCategory {
+
+    private final static int REVERSE_PROBABILITY = 1296;
+
+    public static int getQuantumScore(GivenRoll roll){
+        return (REVERSE_PROBABILITY)
+                *
+                Yatzy.yatzy(
+                        roll.getFirstDice(),
+                        roll.getSecondDice(),
+                        roll.getThirdDice(),
+                        roll.getFourthDice(),
+                        roll.getFifthDice()
+                        );
+    }
+
+}
