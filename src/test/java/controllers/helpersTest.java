557d11e20cb0c59ad2a99e4f38688b05add264e5
Eli Sall <elimane.sall@telecomnancy.net>
2019-04-15 20:25:13 +0200

    tests for the helper controller

---
+++ src/test/java/controllers/helpersTest.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import domain.GivenRoll;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class helpersTest {
+
+    @Test
+    public void getNumberOfRepetition_should_return_the_right_values() {
+        //Given
+        GivenRoll roll = GivenRoll.builder()
+                                    .firstDice(1)
+                                    .secondDice(1)
+                                    .thirdDice(3)
+                                    .fourthDice(4)
+                                    .fifthDice(5)
+                                    .build();
+
+        //Then
+        assertEquals(2, helpers.getNumberOfRepetition(1,  roll));
+        assertEquals(1, helpers.getNumberOfRepetition(3,  roll));
+        assertEquals(0, helpers.getNumberOfRepetition(6,  roll));
+    }
+
+
+    @Test
+    public void getTheMaximumQuantumScoreFromARoll_should_return_the_right_value() {
+        //Given
+        GivenRoll roll = GivenRoll.builder()
+                .firstDice(1)
+                .secondDice(1)
+                .thirdDice(1)
+                .fourthDice(1)
+                .fifthDice(1)
+                .build();
+        //Then
+        assertEquals(64800, helpers.getTheMaximumQuantumScoreFromARoll(roll), 0.001);
+    }
+}
